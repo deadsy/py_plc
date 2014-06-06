b562128980cd32a40f5d8fade5a472ffeb1422f4
Jason Harris <jth@mibot.com>
2014-06-06 08:51:17 -0700

    Output of minimised logic with plc names.

--- logic.py
+++ logic.py
@@ -4,6 +4,8 @@
 from pyeda.inter import *
 import fsm
 
+#------------------------------------------------------------------------------
+
 def bin_tuple(x, n):
     l = []
     for bit in range(n):
@@ -20,6 +22,7 @@ def main():
     f0 = []
     f1 = []
 
+    # run the state machine for all state and input combinations
     for i in range(n):
         v = (bin_tuple(i, bits))
         sv = v[0:fsm.state_bits]
@@ -27,23 +30,32 @@ def main():
         next_sv = fsm.fsm(sv, iv)
         f0.append('%d' % next_sv[0])
         f1.append('%d' % next_sv[1])
-        print(sv, iv, next_sv)
+        print('%s %s -> %s' % (sv, iv, next_sv))
 
-    X = ttvars('x', bits)
+    # setup the function value strings
     f0 = ''.join(f0)
     f1 = ''.join(f1)
+
+    # setup the truth tables
+    X = ttvars('x', bits)
     f0 = truthtable(X, f0)
     f1 = truthtable(X, f1)
 
+    # minimise
     f0m, f1m = espresso_tts(f0, f1)
 
-    print(f0m)
-    print(f1m)
+    # dump the minimised function strings in plc variable form
+    m1_next = str(f0m)
+    m2_next = str(f1m)
 
+    # do the x[] -> plc name replace
+    x2plc = ('i4','i3','i2','i1','m2','m1')
+    for i in range(len(x2plc)):
+        m1_next = m1_next.replace('x[%d]' % i, x2plc[i])
+        m2_next = m2_next.replace('x[%d]' % i, x2plc[i])
 
-
-
-
+    print('m1_next = %s' % m1_next)
+    print('m2_next = %s' % m2_next)
 
 main()
 
